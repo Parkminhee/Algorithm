17645bbe64b9f0559bc9864c8a84e30b5e2c4981
Parkminhee <pminhee114@naver.com>
2020-10-22 16:06:15 +0900

    Create 3진법 뒤집기.cpp

---
+++ PROGRAMMERS/Level 1/3진법 뒤집기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    
+    while(n) {
+        v.push_back(n % 3);
+        n /= 3;
+    }
+    
+    reverse(v.begin(), v.end());
+    
+    for(int i = 0; i < v.size(); i++) {
+        answer += v[i] * pow(3, i);
+    }
+    
+    return answer;
+}
