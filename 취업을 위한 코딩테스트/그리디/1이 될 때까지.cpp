e01d9dbbc51c8fecb202ab5037df740675477c44
Parkminhee <pminhee114@naver.com>
2020-10-25 18:55:14 +0900

    Create 1이 될 때까지.cpp

---
+++ 취업을 위한 코딩테스트/그리디/1이 될 때까지.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int N, K;
+
+int main() {
+	clock_t start, end;
+	double result;
+	int count = 0;
+
+	cin >> N >> K;
+
+	start = clock();
+
+	while (true) {
+		if (N == 1) break;
+
+		if (N % K == 0) N /= K;
+		else N -= 1;
+
+		count += 1;
+	}
+
+	cout << count << endl;
+
+	end = clock();
+	result = (double)(end - start) / CLOCKS_PER_SEC;
+
+	cout << result << endl;
+
+	return 0;
+}
