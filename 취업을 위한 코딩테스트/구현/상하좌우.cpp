b68f7bf1d7a79f61617edb0c76425ec95e749a0e
Parkminhee <pminhee114@naver.com>
2020-11-02 11:45:48 +0900

    Create 상하좌우.cpp

---
+++ 취업을 위한 코딩테스트/구현/상하좌우.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N;
+int R = 1, C = 1;
+string dir;
+
+char d[] = { 'U','D','L','R' };
+int dr[] = { -1, 1, 0, 0 };
+int dc[] = { 0, 0, -1, 1 };
+
+int main() {
+	cin >> N;
+	cin.ignore();		// 버퍼 비우기
+	getline(cin, dir);	// 한 줄 전체 입력받기
+
+	for (int i = 0; i < dir.length(); i++) {
+		char c = dir[i];
+
+		int nr = 0, nc = 0;
+		for (int j = 0; j < 4; j++) {
+			if (c == d[j]) {
+				nr = R + dr[j];
+				nc = C + dc[j];
+				break;
+			}
+		}
+		if (nr < 1 || nc < 1 || nr > N || nc > N) continue;
+		R = nr;
+		C = nc;
+	}
+
+	cout << R << " " << C << endl;
+
+	return 0;
+}
