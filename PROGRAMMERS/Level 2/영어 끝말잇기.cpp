d7a0a18f6edd8ef9dba31ffa64f3b0a845802bb6
Parkminhee <pminhee114@naver.com>
2020-11-20 13:54:07 +0900

    Update 영어 끝말잇기.cpp

--- PROGRAMMERS/Level 2/영어 끝말잇기.cpp
+++ PROGRAMMERS/Level 2/영어 끝말잇기.cpp
@@ -1,3 +1,4 @@
+/*
 #include <string>
 #include <vector>
 #include <map>
@@ -41,6 +42,42 @@ vector<int> solution(int n, vector<string> words) {
     
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
     //cout << "Hello Cpp" << endl;
+
+    return answer;
+}
+*/
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+    map<string, int> m;
+    bool flag = false;
+    int turn = 1;
+    
+    m[words[0]] += 1;
+    
+    for(int i = 1; i < words.size(); i++) {
+        if(i % n == 0) turn += 1;
+        
+        if(m[words[i]] == 1 || (words[i - 1][words[i - 1].length() - 1] != words[i][0])) {
+            answer.push_back((i % n) + 1);
+            answer.push_back(turn);
+            flag = true;
+            break;
+        }
+        m[words[i]] += 1;
+    }
+    
+    if(!flag) {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
 
     return answer;
 }
